e60cffe2881069c3f4666652bfb619c62dd4d77e
Florent P <florent.proust@wanadoo.fr>
2025-09-08 16:50:20 +0200

    Repare path fail

--- tests/ml_pipeline_test.py
+++ tests/ml_pipeline_test.py
@@ -3,7 +3,7 @@ import os
 from unittest import mock
 from app.train import load_data, preprocess_data, create_pipeline, train_model
 
-file_key = 'housing_prices/train_dataset/real_estate_dataset.cs'
+file_key = 'housing_prices/train_dataset/real_estate_dataset.csv'
 
 # Test data loading
 def test_load_data():
